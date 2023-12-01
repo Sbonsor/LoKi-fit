Removed cropping of data so all data is 6d. Set last element of covariance matrix to zero in order to fix the value of epsilon.

dimensional_data_generation.py
@@ -140,11 +140,11 @@ class dimensional_data_generation:
             integrand = r**2*rho
             return np.trapz(y = integrand, x = r)
         
-        # fig1,ax1 = plt.subplots(1,1)
-        # ax1.plot(self.model.rhat,self.model.rhat**2 * self.model.rho_hat/density_normalisation(self.model.rhat,self.model.rho_hat))
-        # ax1.hist(np.sqrt(self.dimensionless_samples[:,0]**2 + self.dimensionless_samples[:,1]**2 + self.dimensionless_samples[:,2]**2), density = True, bins = 100)
-        # ax1.set_xlabel('$\hat{r}$')
-        # ax1.set_ylabel('Radius probability')
+        fig1,ax1 = plt.subplots(1,1)
+        ax1.plot(self.model.rhat,self.model.rhat**2 * self.model.rho_hat/density_normalisation(self.model.rhat,self.model.rho_hat))
+        ax1.hist(np.sqrt(self.dimensionless_samples[:,0]**2 + self.dimensionless_samples[:,1]**2 + self.dimensionless_samples[:,2]**2), density = True, bins = 100)
+        ax1.set_xlabel('$\hat{r}$')
+        ax1.set_ylabel('Radius probability')
         
         self.m = self.M/self.N
         K_i = 0.5 * self.m * (self.vx**2 + self.vy**2 + self.vz**2)

processing_samples.py
@@ -28,8 +28,8 @@ def means_covariace_information_matrix(samples):
     
     return means, cov_matrix, information_matrix
 
-#samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_with_incomplete_data_and_mu.txt')
-samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0_epsilon_1e-06_N_20000_with_incomplete_data_and_mu.txt')
+samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_with_incomplete_data_and_mu.txt')
+#samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0_epsilon_1e-06_N_20000_with_incomplete_data_and_mu.txt')
 
 
 burn_in = 0
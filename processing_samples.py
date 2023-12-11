59a1cf34e1d3248e4d1b0bcaa466446d28e00f4f
Samuel Bonsor <s1984454@ed.ac.uk>
2023-12-11 15:04:09 +0000

    Added script to take rho0 as a fitting  parameter

--- processing_samples.py
+++ processing_samples.py
@@ -29,14 +29,15 @@ def means_covariace_information_matrix(samples):
     return means, cov_matrix, information_matrix
 
 #samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_with_incomplete_data_and_mu.txt')
-samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_fixed_eps_physical_mu.txt')
+#samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_fixed_eps_physical_mu.txt')
 #SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_fixed_eps_physical_mu
 #samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0_epsilon_1e-06_N_20000_with_incomplete_data_and_mu.txt')
-
+samples = np.loadtxt('Data/SAMPLES_parallel_dimensional_samples_King_M_500_rK_1.2_Psi_5_mu_0.3_epsilon_0.1_N_20000_with_incomplete_data_and_mu.txt')
 
 burn_in = 25000
 #stack_samp = np.stack(samples[burn_in:,:])
-stack_samp = np.stack(samples[burn_in:, 0:4])
+#stack_samp = np.stack(samples[burn_in:, 0:4])
+stack_samp = np.stack(samples[burn_in:, 0:3])
 
 #fig = corner.corner(stack_samp, labels = ['$\\Psi$','$a_0$','$M$', '$r_k$'], quantiles = [0.5])
 # plt.savefig('Data/FIG_parallel_' + fname)
@@ -45,7 +46,9 @@ stack_samp = np.stack(samples[burn_in:, 0:4])
 
 #fig = corner.corner(stack_samp, labels = ['$M$','$r_K$','$\\Psi$', '$\\mu$'], quantiles = [0.5])
 
-fig = corner.corner(stack_samp, labels = ['$Ae$','$r_K$','$M_{BH}$', '$\\Psi$'], quantiles = [0.5])
+#fig = corner.corner(stack_samp, labels = ['$Ae$','$r_K$','$M_{BH}$', '$\\Psi$'], quantiles = [0.5])
+
+fig = corner.corner(stack_samp, labels = ['$M$','$r_K$', '$\\Psi$'], quantiles = [0.5])
 
 
 # M_samples = samples[:,0]

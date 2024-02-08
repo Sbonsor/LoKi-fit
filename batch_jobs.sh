02960b0e571f9d892f311bf16dcccf162f7cc986
Samuel Bonsor <s1984454@ed.ac.uk>
2024-02-08 14:52:56 +0000

    Clena up of directory, comparison of M or rho as fitting variables

--- batch_jobs.sh
+++ batch_jobs.sh
@@ -1,27 +1,9 @@
 #!/bin/sh
 
-#mpiexec -n 10 './run_1.py'
-
-#echo 'In between runs now!'
-
-#mpiexec -n 10 './run_2.py'
-
-#echo 'In betwen runs now!'
-
-mpiexec -n 10 './run_3.py'
+mpiexec -n 10 './run_9.py'
 
 echo 'In between runs now!'
 
-mpiexec -n 10 './run_4.py'
+mpiexec -n 10 './run_10.py'
 
-echo 'In between runs now!'
 
-mpiexec -n 10 './run_3a.py'
-
-echo 'In between runs now!'
-
-mpiexec -n 10 './run_4a.py'
-
-echo 'In between runs now!'
-
-mpiexec -n 10 './run_5.py'
